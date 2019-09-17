d62ffa932cbfca0e59e00e4aed8e96a143d16aa3
Peter Halliday <pghalliday@gmail.com>
2019-09-17 17:03:49 +0200

    tidying

--- options.js
+++ options.js
@@ -7,19 +7,3 @@ chrome.storage.local.get('extracted', function(items) {
     });
   }
 });
-
-let page = document.getElementById('buttonDiv');
-const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-function constructOptions(kButtonColors) {
-  for (let item of kButtonColors) {
-    let button = document.createElement('button');
-    button.style.backgroundColor = item;
-    button.addEventListener('click', function() {
-      chrome.storage.sync.set({color: item}, function() {
-        console.log('color is ' + item);
-      })
-    });
-    page.appendChild(button);
-  }
-}
-constructOptions(kButtonColors);
